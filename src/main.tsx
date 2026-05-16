0424c5d3180533dfa1bc00ecb66572c8b41ee4a7
Sethika-manu <dinilkumara061@gmail.com>
2026-05-16 20:57:01 +0530

    .

--- src/main.tsx
+++ src/main.tsx
@@ -9,5 +9,5 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <SettingsProvider>
       <App />
     </SettingsProvider>
-  </React.StrictMode>
+  </React.StrictMode> //all
 );
